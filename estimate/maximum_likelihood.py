792bfc5fb0292808500399f36cb98caf1ddc14d1
dhwang99 <dhwang99@163.com>
2017-07-25 14:35:55 +0800

    update maxinum likelihood

--- estimate/maximum_likelihood.py
+++ estimate/maximum_likelihood.py
@@ -32,7 +32,7 @@ def ML_for_Exp(n, theta):
     samples = np.random.exponential(theta, n)
     theta_head = samples.mean()
 
-    return theta_head 
+    return (theta_head)
 
 
 '''
@@ -52,7 +52,7 @@ def ML_for_Bernolli(n, p):
     sum_one = samples.sum()
     p_head = sum_one * 1. / n
 
-    return p_head
+    return (p_head)
 
 '''
 1. 
@@ -60,45 +60,37 @@ def ML_for_Bernolli(n, p):
    L = PI(f(xi; mu, sigma)
 2. 
    l(theta) = sum(log(1./(sqrt(2pi)*sigma)) - sum((xi-mu)^2/(2*sigma^2)
+            = sum(log(1/sqrt(2pi) -1/2*log(sigma^2) - sum((xi-mu)^2/(2*sigma^2)
 
 3. deriv:
     sum(xi - mu) / (2*sigma^2) = 0
     mu = sum(xi)/n
+
+    partial(l)/partial(sigma^2) = -n/2*1/(sigma^2) + sum((xi-mu)^2) / 2*sigma^4 = 0
+    sigma^2 = sum(xi-mu)^2/n   #非无偏估计
 '''
 def ML_for_Norm_mu(n, mu, sigma):
     samples = np.random.normal(mu, sigma, n)
     mu_head = samples.mean()
-    return mu_head
 
-'''
-2. log function:
-   l(theta) = sum(log(1./(sqrt(2pi)*sigma)) - sum((xi-mu)^2/(2*sigma^2)
-            = sum(log(1/sqrt(2pi) -1/2*log(sigma^2) - sum((xi-mu)^2/(2*sigma^2)
-
-3. deriv:
-    partial(l)/partial(sigma^2) = -n/2*1/(sigma^2) + sum((xi-mu)^2) / 2*sigma^4 = 0
-    sigma^2 = sum(xi-mu)^2/n   #非无偏估计
-
-'''
-def ML_for_Norm_sigma(n, mu, sigma):
-    samples = np.random.normal(mu, sigma, n)
-    v = samples - samples.mean()
+    v = samples - mu_head
     v2 = np.dot(v, v) / n
     v2 = np.sqrt(v2)
 
-    return v2 
+    return (mu_head, v2)
 
 def ML_test(test_count, l_fun, sample_count, *param):
-    errors = np.zeros(test_count)
-    thetas = np.zeros(test_count)
-    for i in xrange(test_count):
-        theta_head = l_fun(sample_count, *param)
-        
-        errors[i] = param[0] - theta_head
-        thetas[i] = theta_head
+    es_num = len(param)
+    errors = np.zeros((test_count, es_num))
+    thetas = np.zeros((test_count, es_num))
 
-    
-    print "real param: %s; estimate mean: %s; var: %s" % (param, thetas.mean(), thetas.var())
+    for i in xrange(test_count):
+        thetas_head = l_fun(sample_count, *param)
+        errors[i, :] = np.array(param) - thetas_head
+        thetas[i, :] = thetas_head
+
+    for i in xrange(es_num):
+        print "real param (%s,%s): %s; estimate: %s; var: %s" % (es_num, i, param[i], thetas[:,i].mean(), thetas[:,i].var())
 
 if __name__ == "__main__":
     test_count = 10
@@ -112,7 +104,4 @@ if __name__ == "__main__":
     
     print '\nmaximum likelihood for Normal distribute samples: Mu:'
     ML_test(test_count, ML_for_Norm_mu, sample_count, 2, 3)
-    
-    print '\nmaximum likelihood for Normal distribute samples: Var:'
-    ML_test(test_count, ML_for_Norm_sigma, sample_count, 2, 3)
     
