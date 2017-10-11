2a88c2acb5c732f6d4960faa5ad7d28ee08260b2
dhwang99 <dhwang99@163.com>
2017-10-11 18:14:35 +0800

    add statistics learning

---
+++ learning/beta_dist.py
@@ -0,0 +1,101 @@
+#encoding: utf8
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import beta
+import pdb
+
+'''
+
+Beta 分布
+
+'''
+
+'''
+当a,b有一个为1时，为单调函数, 两个都为1时，则为[0,1]间的均匀分布
+
+f(x;a,b) = x^(a-1)*(1-x)^(b-1)/B(a,b)
+B(a,b) = Gamma(a)Gamma(b)/Gamma(a+b-1)
+
+E(B) = a/(a+b)
+V(B) = ab/[(a+b)^2(a+b+1)]
+
+假设我们预计运动员整个赛季的击球率大概是0.27左右，范围大概是在0.21到0.35之间。那么用贝塔分布来表示，我们可以取参数 α==81，β==219。
+为什么估计为这个结果？下面的算法好象也不靠谱
+B_bar = 0.27
+se_hat(B) = 0.024 (这个是瞎算的。)
+
+b = 3a
+a = (3/(16 * V) - 1)/4
+
+应该是去年打了300次，有81次中，219次没有中。于是有了这个先验
+'''
+
+gamma_values = {}
+
+'''
+gamma(0.5) = sqrt(pi)
+gamma(1) = 1
+
+gamma(alpha) = (alpha - 1) gamma(alpha - 1)
+'''
+def init_gamma_values():
+    gamma_values[0] = 1e10   #sim to infty
+    gamma_values[0] = 10000
+    gamma_values[0.5] = np.sqrt(np.pi)
+    gamma_values[1] = 1
+    
+    for i in range(1, 1000):
+        a1 = i - 0.5
+        a2 = i 
+
+        gamma_values[a1+1] = a1 * gamma_values[a1]
+        gamma_values[a2+1] = a2 * gamma_values[a2]
+
+init_gamma_values()
+
+def beta_pdf(x, a, b):
+    gamma_a = gamma_values[a] 
+    gamma_b = gamma_values[b] 
+    gamma_ab = gamma_values[a+b] 
+
+    pdf = np.power(x, a-1) * np.power(1-x, b- 1) * gamma_ab / gamma_a / gamma_b
+    
+    return pdf
+
+def draw_beta_dist(ab_list, fname):
+    colors = ['r', 'b', 'k', 'g', 'm', 'c']
+    ls = []
+    ab_lables = map(lambda x:'%s:%s' % x, ab_list) 
+
+    for i in range(len(ab_list)):
+        a,b = ab_list[i]
+        points = np.linspace(0., 1., 101)
+        '''
+        pdfs = map(lambda x:beta_pdf(x, a,b), points)
+        l, = plt.plot(points, pdfs, color=colors[i%len(colors)])
+        '''
+
+        pdfs = map(lambda x:beta.pdf(x, a,b), points)
+        l, = plt.plot(points, pdfs, color=colors[i%len(colors)])
+        ls.append(l)
+    
+
+    plt.legend(handles = ls, labels = ab_lables, loc = 'best')
+    plt.savefig(fname, format='png')
+
+if __name__ == '__main__':
+    ab_list = [(0.5, 0.5), (5, 2), (1, 3), (2, 2), (3,1), (2,5), (5,5)]
+    fname = 'images/beta01.png'
+    draw_beta_dist(ab_list, fname)
+    
+    plt.clf()
+    ab_list = [(20, 219), (81, 219), (150, 219), (219, 219), (500, 219), (1000,219)]
+    fname = 'images/beta02.png'
+    draw_beta_dist(ab_list, fname)
+
+    plt.clf()
+    ab_list = [(5, 1), (5, 3), (5, 5), (5, 7), (5,10), (5, 20)]
+    fname = 'images/beta03.png'
+    draw_beta_dist(ab_list, fname)
+    
---
+++ learning/ex.py
@@ -0,0 +1,51 @@
+#encoding: utf8
+
+import numpy as np
+import pdb
+
+'''
+load data from file
+return:
+    train src data, test src data, normed training src dtaa, normed test src data
+'''
+def data_convert(val):
+    if val == 'T':
+        return 1
+    elif val == 'F':
+        return 2
+    return 0
+
+'''
+问题1：
+1. 关于标准化，是每一个因子在全集上标准化，还只是在测试集上标标准化？
+我用的是在全集上标准化，这个应该更合理一些
+
+问题2：
+2. 在求协方差时，np.cov的算法：sum(Xi-X_mean)(Yi-Y_mean)/(len(samples)-1)
+   在实际上我算的时候，并没有减1. 这两个是不是第1个更准(无偏)但实际上用的时候是第2个?
+   mean_data.T * mean_data / n
+
+'''
+def load_data(filename='data/prostate.data.txt'):
+    usecolums = range(1, 11)
+    alldata = np.loadtxt(filename, skiprows=1, usecols=usecolums, converters={10:data_convert})
+    mean_val = alldata.mean(axis=0)
+    mean_data = np.copy(alldata)
+    mean_data = (mean_data[:,0:9] - mean_val[0:9])
+    pdb.set_trace()
+    mean_data = mean_data / mean_val[0:9]
+
+    t_indicate = np.where(alldata[:,9] == 1)
+    f_indicate = np.where(alldata[:,9] == 2)
+    
+    t_data = np.take(alldata, t_indicate[0], axis=0)[:,0:9]
+    f_data = np.take(alldata, f_indicate[0], axis=0)[:,0:9]
+
+    t_mean_data = np.take(mean_data, t_indicate[0], axis=0)
+    f_mean_data = np.take(mean_data, f_indicate[0], axis=0)
+
+    return t_data, f_data, t_mean_data, f_mean_data
+     
+
+if __name__ == '__main__':
+    td, fd, tmd, fmd = load_data()
